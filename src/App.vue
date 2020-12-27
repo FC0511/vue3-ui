ac5211e368f166baad5727ba6dbe3ed8b9e83d77
fc <1032733797@qq.com>
2020-12-27 09:18:32 +0800

    添加toggleAside 按钮在手机端切换显示

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,10 @@ import {ref, provide} from 'vue'
 export default {
   name: 'App',
   setup() {
+    // 获取页面宽度
+    const winWidth = document.documentElement.clientWidth;
     // ref函数只能监听简单类型的变化  简单值使用ref,复杂值使用reactive
-    const asideVisible = ref(false);
+    const asideVisible = ref(winWidth > 500);
     provide('asideVisible', asideVisible)
   }
 }
